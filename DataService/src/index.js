e82665a64d1bc758900f3a8552a2f14c3475832c
Dusan <dule.sotirov99@gmail.com>
2026-02-13 22:51:34 +0100

    add MongoDb integration and persistence in DataServjce

--- DataService/src/index.js
+++ DataService/src/index.js
@@ -1,24 +1,89 @@
 const express = require('express');
+const {connectDb, getDb} = require('./db');
+const {ObjectId} = require('mongodb');
 
 const app = express();
-
 app.use(express.json());
 
-app.get('/health', (req, res) => {
-    res.json({ status: 'Ok', service: "DataService" });
+//#region Checking DB connection
+
+app.get('/health', async (req, res) => {
+    try{
+        const db = getDb();
+        await db.command({ ping: 1 });
+        res.json({ status: 'Ok', service: "DataService", mongo: 'Connected' });
+    }
+    catch{
+        res.status(500).json({ status: 'Error', service: "DataService", mongo: 'Not Connected' });
+    }
 });
 
-app.post("/readings", (req, res) => {
-    const readings = req.body;
+//#endregion Checking DB connection
 
-    console.log("Received readings:", readings);
+//#region Post
 
-    res.status(201).json(readings);
+app.post("/readings", async (req, res) => {
+    const reading = req.body;
+
+    if(reading.utc == null){
+        return res.status(400).json({ error: "Missing 'Utc' field in the request body." });
+    }
+
+    const db = getDb();
+    const collection = db.collection('readings');
+
+    const doc = {
+        ...reading,
+        createdAt: new Date() 
+    };
+    
+    const result = await collection.insertOne(doc);
+
+    res.status(201).json({id: result.insertedId, ...doc});
 });
 
+//#endregion Post
 
-const PORT = 5001;
+//#region Get
 
-app.listen(PORT, () => {
-    console.log(`Data Service is running on port ${PORT}`);
+app.get("/readings", async (req, res) => {
+
+    const db = getDb();
+    const collection = db.collection('readings');
+
+    const items= await collection.find().sort({ utc: -1 }).toArray();
+
+    res.json(items);
 });
+
+app.get("/reading/:id",async (req, res) => {
+    const db = getDb();
+    const collection = db.collection('readings');
+
+    try{
+        const item = await collection.findOne({_id: new ObjectId(req.params.id)});
+        if(!item)
+            return res.status(404).json({error: "Not Found"});
+
+        res.json(item);
+    }
+    catch{
+        res.status(400).json({error: "Invalid ID format"});
+    }
+});
+
+//#endregion Get
+
+const PORT = process.env.PORT || 5001;
+
+connectDb()
+.then(() => {
+    app.listen(PORT, () =>{
+        console.log(`Data Service is running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+});
+
